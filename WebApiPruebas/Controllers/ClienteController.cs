cf87b58e9627f179fc9fc4805d760321a0cf23ab
unNombre <es.ucles@gmail.com>
2022-06-26 19:44:55 +0200

    Cliente Crear

---
+++ WebApiPruebas/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiPruebas.Modelo;
+using WebApiPruebas.Servicios;
+
+namespace WebApiPruebas.Controllers
+{
+    [ApiController]
+    [Route("api/clientes")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IRepositorioClientes repositorioClientes;
+
+        public ClienteController(IRepositorioClientes repositorioClientes)
+        {
+            this.repositorioClientes = repositorioClientes; 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Crear(Cliente cliente)
+        {
+            await repositorioClientes.Crear(cliente);
+            return Ok();
+        }
+
+    }
+}
---
+++ WebApiPruebas/Servicios/RepositorioClientes.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using WebApiPruebas.Modelo;
+
+namespace WebApiPruebas.Servicios
+{
+    public interface IRepositorioClientes
+    {
+        Task Crear(Cliente cliente);
+    }
+
+    public class RepositorioClientes : IRepositorioClientes
+    {
+        private readonly string connectionString;
+
+        public RepositorioClientes(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("defaultConnection");
+        }
+
+        public async Task Crear(Cliente cliente)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>
+                                                    ($@"INSERT INTO Clientes (Nombre)
+                                                     VALUES (@Nombre);
+                                                     SELECT SCOPE_IDENTITY();",cliente);
+
+            cliente.Id = id;
+        }
+
+
+    }
+}
